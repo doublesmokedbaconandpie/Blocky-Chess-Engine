e37175aab4cc701407694c4cb1b79dd7133f9fb5
Kevin Nguyen <kevinhnguyen@ucsb.edu>
2023-08-08 21:37:33 -0700

    add inCheck definition

--- src/board.cpp
+++ src/board.cpp
@@ -16,19 +16,23 @@ Board::Board() {
     this->movesWithoutCapture = 0;
     this->pawnJumped = false;
     this->pawnJumpedSquare = BoardSquare();
+    this->inCheck = false;
 }
 
 Board::Board(std::vector<std::vector<int>> board, bool isWhiteTurn = true, 
                 int movesWithoutCapture = 0, bool pawnJumped = false, 
-                BoardSquare pawnJumpedSquare = BoardSquare()) {
+                BoardSquare pawnJumpedSquare = BoardSquare(), bool inCheck = false) {
     this->board = board;
     this->isWhiteTurn = isWhiteTurn;
     this-> movesWithoutCapture = movesWithoutCapture;
     this->pawnJumped = pawnJumped;
     this->pawnJumpedSquare = pawnJumpedSquare;
+    this->inCheck = inCheck;
 }
 
 Board::Board(Board originalBoard, BoardSquare pos1, BoardSquare pos2) {
+    // does not work for castling
+
     this->board = originalBoard.board;
     int tmp = this->board.at(pos1.rank).at(pos1.file);
     this->board.at(pos1.rank).at(pos1.file) = 0;
@@ -42,16 +46,26 @@ Board::Board(Board originalBoard, BoardSquare pos1, BoardSquare pos2) {
 
 std::vector<Board> moveGenerator(Board currBoard) {
     std::vector<Board> listOfMoves;
-    validPawnMoves(currBoard, listOfMoves);
-    validKnightMoves(currBoard, listOfMoves);
-    validBishopMoves(currBoard, listOfMoves);
-    validRookMoves(currBoard, listOfMoves);
-    validQueenMoves(currBoard, listOfMoves);
-    validKingMoves(currBoard, listOfMoves);
+    std::vector<BoardSquare> pawns, knights, bishops, rooks, queens, kings;
+
+    // get squares where each piece is
+
+    for (int rank = 0; rank < 8; rank++) {
+        for (int file = 0; file < 8; file++)
+    }
+
+    // add logic to find all piece types
+
+    validPawnMoves(currBoard, listOfMoves, pawns);
+    validKnightMoves(currBoard, listOfMoves, knights);
+    validBishopMoves(currBoard, listOfMoves, bishops);
+    validRookMoves(currBoard, listOfMoves, rooks);
+    validQueenMoves(currBoard, listOfMoves, queens);
+    validKingMoves(currBoard, listOfMoves, kings);
     return listOfMoves;
 }
 
-void validPawnMoves(Board& currBoard, std::vector<Board>& validMoves) {
+void validPawnMoves(Board& currBoard, std::vector<Board>& validMoves, std::vector<BoardSquare>& pawns) {
 
     // check 2nd rank for move two spaces instead of 1
 
@@ -63,25 +77,25 @@ void validPawnMoves(Board& currBoard, std::vector<Board>& validMoves) {
 
 }
 
-void validKnightMoves(Board& currBoard, std::vector<Board>& validMoves) {
+void validKnightMoves(Board& currBoard, std::vector<Board>& validMoves, std::vector<BoardSquare>& knights) {
     // iterate through currBoard to find the knights
 }
 
-void validBishopMoves(Board& currBoard, std::vector<Board>& validMoves) {
+void validBishopMoves(Board& currBoard, std::vector<Board>& validMoves, std::vector<BoardSquare>& bishops) {
     // iterate through currBoard to find the bishops
 }
 
-void validRookMoves(Board& currBoard, std::vector<Board>& validMoves) {
+void validRookMoves(Board& currBoard, std::vector<Board>& validMoves, std::vector<BoardSquare>& rooks) {
     // iterate through currBoard to find the rooks
 }
 
-void validQueenMoves(Board& currBoard, std::vector<Board>& validMoves) {
+void validQueenMoves(Board& currBoard, std::vector<Board>& validMoves, std::vector<BoardSquare>& queens) {
     // iterate through currBoard to find the queens (could be more due to promotions)
     // repeat the logic used for rooks and bishops
 }
 
 
-void validKingMoves(Board& currBoard, std::vector<Board>& validMoves) {
+void validKingMoves(Board& currBoard, std::vector<Board>& validMoves, std::vector<BoardSquare>& kings) {
     // get valid king moves
 
     // get castling
--- src/board.hpp
+++ src/board.hpp
@@ -18,21 +18,23 @@ struct Board {
     Board(); // begin default game
     Board(std::vector<std::vector<int>> board, bool isWhiteTurn = true, 
             int movesWithoutCapture = 0, bool pawnJumped = false, 
-            BoardSquare pawnJumpedSquare = BoardSquare()); // maybe useful for testing or chess960
+            BoardSquare pawnJumpedSquare = BoardSquare(), bool inCheck = false); // maybe useful for testing or chess960
     Board(Board originalBoard, BoardSquare pos1, BoardSquare pos2); // creates a new board for a move 
-    
+
     std::vector<std::vector<int>> board;
     bool isWhiteTurn;
     int movesWithoutCapture; // 50 move rule
+    bool inCheck;
     bool pawnJumped;
     BoardSquare pawnJumpedSquare;
 };
 
 std::vector<Board> moveGenerator(Board currBoard); // outputs board instead of board moves for future evaluation functions
-void validPawnMoves(Board& currBoard, std::vector<Board>& validMoves); // includes en passant
-void validKnightMoves(Board& currBoard, std::vector<Board>& validMoves);
-void validBishopMoves(Board& currBoard, std::vector<Board>& validMoves);
-void validRookMoves(Board& currBoard, std::vector<Board>& validMoves);
-void validQueenMoves(Board& currBoard, std::vector<Board>& validMoves);
-void validKingMoves(Board& currBoard, std::vector<Board>& validMoves); // includes castling
+void validPawnMoves(Board& currBoard, std::vector<Board>& validMoves, std::vector<BoardSquare>& pawns); // includes en passant
+void validKnightMoves(Board& currBoard, std::vector<Board>& validMoves, std::vector<BoardSquare>& knights);
+void validBishopMoves(Board& currBoard, std::vector<Board>& validMoves, std::vector<BoardSquare>& bishops);
+void validRookMoves(Board& currBoard, std::vector<Board>& validMoves, std::vector<BoardSquare>& rooks);
+void validQueenMoves(Board& currBoard, std::vector<Board>& validMoves, std::vector<BoardSquare>& queens);
+void validKingMoves(Board& currBoard, std::vector<Board>& validMoves, std::vector<BoardSquare>& kings); // includes castling
 // idk what to do about 3-fold repetition; maybe a hash table
+
