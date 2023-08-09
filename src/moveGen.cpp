1631e76ecb5f343c615392f0fff53e59b78719d3
Kevin Nguyen <kevinhnguyen@ucsb.edu>
2023-08-08 22:39:50 -0700

    new files

--- src/moveGen.cpp
+++ src/moveGen.cpp
@@ -1,48 +1,4 @@
-
-#include "board.hpp"
-
-Board::Board() {
-    this->board = {
-        {BRookUnmoved, BKnight, BBishop, BQueen, BKingUnmoved, BBishop, BKnight, BRookUnmoved},
-        {BPawn, BPawn, BPawn, BPawn, BPawn, BPawn, BPawn, BPawn},
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {WPawn, WPawn, WPawn, WPawn, WPawn, WPawn, WPawn, WPawn},
-        {WRookUnmoved, WKnight, WBishop, WQueen, WKingUnmoved, WBishop, WKnight, WRookUnmoved}
-    };
-    this->isWhiteTurn = true;
-    this->movesWithoutCapture = 0;
-    this->pawnJumped = false;
-    this->pawnJumpedSquare = BoardSquare();
-    this->inCheck = false;
-}
-
-Board::Board(std::vector<std::vector<int>> board, bool isWhiteTurn = true, 
-                int movesWithoutCapture = 0, bool pawnJumped = false, 
-                BoardSquare pawnJumpedSquare = BoardSquare(), bool inCheck = false) {
-    this->board = board;
-    this->isWhiteTurn = isWhiteTurn;
-    this-> movesWithoutCapture = movesWithoutCapture;
-    this->pawnJumped = pawnJumped;
-    this->pawnJumpedSquare = pawnJumpedSquare;
-    this->inCheck = inCheck;
-}
-
-Board::Board(Board originalBoard, BoardSquare pos1, BoardSquare pos2) {
-    // does not work for castling
-
-    this->board = originalBoard.board;
-    int tmp = this->board.at(pos1.rank).at(pos1.file);
-    this->board.at(pos1.rank).at(pos1.file) = 0;
-    this->board.at(pos2.rank).at(pos2.file) = tmp;
-
-    this->isWhiteTurn = !originalBoard.isWhiteTurn;
-
-    // need to add logic to convert pawnJumped, rookUnmoved, and kingUnmoved to their respective pieces
-    // also check for captures
-}
+#include "moveGen.hpp"
 
 std::vector<Board> moveGenerator(Board currBoard) {
     std::vector<Board> listOfMoves;
---
+++ src/moveGen.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "board.hpp"
+#include "inCheck.hpp"
+
+std::vector<Board> moveGenerator(Board currBoard); // outputs board instead of board moves for future evaluation functions
+void validPawnMoves(Board& currBoard, std::vector<Board>& validMoves, std::vector<BoardSquare>& pawns); // includes en passant
+void validKnightMoves(Board& currBoard, std::vector<Board>& validMoves, std::vector<BoardSquare>& knights);
+void validBishopMoves(Board& currBoard, std::vector<Board>& validMoves, std::vector<BoardSquare>& bishops);
+void validRookMoves(Board& currBoard, std::vector<Board>& validMoves, std::vector<BoardSquare>& rooks);
+void validQueenMoves(Board& currBoard, std::vector<Board>& validMoves, std::vector<BoardSquare>& queens);
+void validKingMoves(Board& currBoard, std::vector<Board>& validMoves, std::vector<BoardSquare>& kings); // includes castling
+// idk what to do about 3-fold repetition; maybe a hash table
