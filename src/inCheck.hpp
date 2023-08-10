71a72c099cb600ccfda4d3eae3840c99815794c9
Kevin Nguyen <kevinhnguyen@ucsb.edu>
2023-08-10 14:26:06 -0700

    don't remove addMovesInDirection in this pull request yet

--- src/inCheck.hpp
+++ src/inCheck.hpp
@@ -7,6 +7,7 @@
 // checkPiece doesn't check if the piece and targetKing are on the same side
 
 bool isFriendlyPiece(Board& currBoard, BoardSquare targetSquare);
+void addMovesInDirection(Board& currBoard, std::vector<BoardSquare>& movesVec, BoardSquare originSquare, int rankIncrement, int fileIncrement);
 pieceTypes getPieceInDirection(Board& currBoard, BoardSquare originSquare, int rankIncrement, int fileIncrement);
 
 bool checkDiagAttackers(Board& currBoard, BoardSquare originSquare, pieceTypes originPiece);
