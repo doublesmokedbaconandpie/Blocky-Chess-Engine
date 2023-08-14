f974b10f344184d500188158c0d2ab414f5a1539
Kevin Nguyen <kevinhnguyen@ucsb.edu>
2023-08-13 18:10:13 -0700

    shell stuff and CMakeFile

--- runTests.sh
+++ runTests.sh
@@ -1,3 +1,11 @@
+verbose=""
+
+while getopts 'va:' option; do
+    case $option in 
+        v) verbose="-V";;
+    esac
+done
+
 cmake -S . -B build
 cmake --build build
-( cd build ; ctest -V )
+( cd build ; ctest ${verbose} )
