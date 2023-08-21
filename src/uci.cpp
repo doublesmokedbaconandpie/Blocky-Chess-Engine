39b0d70490b65a062539bd43922ea7f409cbf278
Kevin Nguyen <kevinhnguyen@ucsb.edu>
2023-08-20 23:19:51 -0700

    uci implementation of fen

--- src/uci.cpp
+++ src/uci.cpp
@@ -72,9 +72,10 @@ namespace UCI {
 
             if (commandToken == "ucinewgame") {}
             else if (commandToken == "position") {currBoard = position(commandStream);}
-            else if (commandToken == "go") {go(commandStream, currBoard);}
+            else if (commandToken == "go") {UCI::go(commandStream, currBoard);}
             else if (commandToken == "isready") {isready();}
             else if (commandToken == "quit") {return;}
+            std::cout << currBoard << std::endl;
         }
     }
 
@@ -86,15 +87,19 @@ namespace UCI {
         input >> token;
         if (token == "startpos") {
             currBoard = Board();
+            input >> token;
         }
         else if (token == "fen") {
-            throw std::invalid_argument("NOT FEN");
+            std::string fenStr;
+            while (input >> token && token != "moves") { 
+                fenStr += token + ' ';
+            }
+            currBoard = Board(fenStr);
         }
 
-        input >> token;
         if (token != "moves") {return currBoard;}
         while (input >> token) {
-            currBoard = Board(currBoard, BoardMove(token));
+            currBoard = Board(currBoard, BoardMove(token, currBoard.isWhiteTurn));
         }
         return currBoard;
     }
