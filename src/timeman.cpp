2cb71ccf37e0f15455c6a5b5ea4acf8f9f668724
Kevin Nguyen <kevinhnguyen@ucsb.edu>
2023-08-21 20:00:15 -0700

    change time bounds since previous time bounds are weaker

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -1,8 +1,8 @@
 #include "timeman.hpp"
 
 int negamaxDepthBasedOnTime(int ms) {
-    if (ms >= 30000) {return 4;}
-    if (ms >= 10000) {return 3;}
-    if (ms >= 2000) {return 2;}
+    if (ms >= 35000) {return 4;}
+    if (ms >= 2000) {return 3;}
+    if (ms >= 500) {return 2;}
     return 1;
 }
