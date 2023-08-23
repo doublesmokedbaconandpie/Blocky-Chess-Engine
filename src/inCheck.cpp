233251d2607c1883638d459af4509a3bcc3fef8a
Kevin Nguyen <kevinhnguyen@ucsb.edu>
2023-08-23 14:25:21 -0700

    correct file

--- src/inCheck.cpp
+++ src/inCheck.cpp
@@ -229,7 +229,7 @@ Board::Board(Board& originalBoard, BoardSquare pos1, BoardSquare pos2, pieceType
     }
     else if (originPiece == allyRook) {
         this->setPiece(pos2, originPiece);
-        this->castlingRights &= pos2.rank == H ? K_Castle : Q_Castle;
+        this->castlingRights &= pos2.rank == G ? K_Castle : Q_Castle;
     }
     // jumping pawn
     else if (originPiece == allyPawn && pos2.rank == pos1.rank + pawnJumpDirection) { 
