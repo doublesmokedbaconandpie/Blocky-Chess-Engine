4a2b18c6e0af6e19fac7f5741e08d8aeddb33d20
altoclef <72427115+charlessu800@users.noreply.github.com>
2023-09-01 20:50:22 -0700

    block exe cmake work

---
+++ CMakeFiles/Blocky.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Charles/Documents/GitHub/Blocky-Chess-Engine/src/bitboard.cpp" "CMakeFiles/Blocky.dir/src/bitboard.cpp.obj" "gcc" "CMakeFiles/Blocky.dir/src/bitboard.cpp.obj.d"
+  "C:/Users/Charles/Documents/GitHub/Blocky-Chess-Engine/src/board.cpp" "CMakeFiles/Blocky.dir/src/board.cpp.obj" "gcc" "CMakeFiles/Blocky.dir/src/board.cpp.obj.d"
+  "C:/Users/Charles/Documents/GitHub/Blocky-Chess-Engine/src/eval.cpp" "CMakeFiles/Blocky.dir/src/eval.cpp.obj" "gcc" "CMakeFiles/Blocky.dir/src/eval.cpp.obj.d"
+  "C:/Users/Charles/Documents/GitHub/Blocky-Chess-Engine/src/main.cpp" "CMakeFiles/Blocky.dir/src/main.cpp.obj" "gcc" "CMakeFiles/Blocky.dir/src/main.cpp.obj.d"
+  "C:/Users/Charles/Documents/GitHub/Blocky-Chess-Engine/src/move.cpp" "CMakeFiles/Blocky.dir/src/move.cpp.obj" "gcc" "CMakeFiles/Blocky.dir/src/move.cpp.obj.d"
+  "C:/Users/Charles/Documents/GitHub/Blocky-Chess-Engine/src/moveGen.cpp" "CMakeFiles/Blocky.dir/src/moveGen.cpp.obj" "gcc" "CMakeFiles/Blocky.dir/src/moveGen.cpp.obj.d"
+  "C:/Users/Charles/Documents/GitHub/Blocky-Chess-Engine/src/search.cpp" "CMakeFiles/Blocky.dir/src/search.cpp.obj" "gcc" "CMakeFiles/Blocky.dir/src/search.cpp.obj.d"
+  "C:/Users/Charles/Documents/GitHub/Blocky-Chess-Engine/src/timeman.cpp" "CMakeFiles/Blocky.dir/src/timeman.cpp.obj" "gcc" "CMakeFiles/Blocky.dir/src/timeman.cpp.obj.d"
+  "C:/Users/Charles/Documents/GitHub/Blocky-Chess-Engine/src/uci.cpp" "CMakeFiles/Blocky.dir/src/uci.cpp.obj" "gcc" "CMakeFiles/Blocky.dir/src/uci.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
