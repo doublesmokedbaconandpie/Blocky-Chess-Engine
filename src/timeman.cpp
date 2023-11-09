e714a353cf475ec76d6370d5e8477d5f79ac1801
Kevin Nguyen <kevinhnguyen@ucsb.edu>
2023-11-08 18:59:36 -0800

    clean up timeman

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -1,6 +1,8 @@
-#include "timeman.hpp"
 #include <iostream>
 #include <cstdint>
+
+#include "timeman.hpp"
+
 namespace Timeman {
 
 bool TimeManager::timeUp() const {
@@ -8,7 +10,7 @@ bool TimeManager::timeUp() const {
     return std::chrono::duration_cast<std::chrono::microseconds>(currTime - startTime).count() > timeLimit;
 }
 
-uint64_t TimeManager::getTimeElapsed() const {
+int64_t TimeManager::getTimeElapsed() const {
     auto currTime = std::chrono::high_resolution_clock::now();
     return std::chrono::duration_cast<std::chrono::milliseconds>(currTime - startTime).count();
 }
--- src/timeman.hpp
+++ src/timeman.hpp
@@ -9,22 +9,16 @@ constexpr int INF_TIME = 1000000000;
 
 struct TimeManager
 {
-    // We need this for search.hpp to not die
-    TimeManager() {
-        startTime = std::chrono::high_resolution_clock::now();
-        timeLimit = INF_TIME;
-    }
-
-    TimeManager(int ms) {
-        startTime = std::chrono::high_resolution_clock::now();
-        timeLimit = ms * 1000 / 20;
+    TimeManager(int ms = INF_TIME) {
+        this->startTime = std::chrono::high_resolution_clock::now();
+        this->timeLimit = ms * 1000 / 20;
     }
 
     bool timeUp() const;
-    uint64_t getTimeElapsed() const;
+    int64_t getTimeElapsed() const;
     
     std::chrono::system_clock::time_point startTime; 
-    uint64_t timeLimit;
+    int64_t timeLimit;
 };
 
 } // namespace Timeman
