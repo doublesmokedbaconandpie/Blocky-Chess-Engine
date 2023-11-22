a84b5823b59219e0365efa67c8290b9c1a4e3a5b
Kevin Nguyen <kevinhnguyen@ucsb.edu>
2023-11-22 10:27:45 -0800

    simplify

--- src/moveGen.hpp
+++ src/moveGen.hpp
@@ -28,14 +28,6 @@ uint64_t rookMoves(int square, MoveGenInfo& info);
 uint64_t kingMoves(int square, MoveGenInfo& info);
 uint64_t pawnMoves(int square, MoveGenInfo& info, bool isWhiteTurn);
 
-// indexes based on castle rights defined in "types.hpp"
-constexpr std::array<uint64_t, 4> rookPaths = {
-    0x6000000000000000ull, 0x0E00000000000000ull, 0x0000000000000060ull, 0x000000000000000Eull};
-constexpr std::array<uint64_t, 4> kingPaths = {
-    0x2000000000000000ull, 0x0800000000000000ull, 0x0000000000000020ull, 0x0000000000000008ull};
-constexpr std::array<uint64_t, 4> castleDestination = {
-    0x4000000000000000ull, 0x0400000000000000ull, 0x0000000000000040ull, 0x0000000000000004ull};
-
 // for debugging
 uint64_t perft(Board board, int depthLeft);
 uint64_t perftHelper(Board board, int depthLeft);
