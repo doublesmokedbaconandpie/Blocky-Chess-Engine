dbe689ba90b3c6f02e399c68decf8e96bc023e38
Kevin Nguyen <kevinngguyen1@gmail.com>
2023-12-30 22:16:34 -0800

    check time every 1024 nodes (#173)
    This checks system time every 1024 nodes instead, which helps with performance.
    
    ```
    Advancement Test:
    Time Control: 10s + 0.1s
    Games: N=1044 W=385 L=302 D=357
    Elo: 27.7 +/- 17.1
    Bench: 8243970
    
    Alpha: 0.05
    Beta: 0.05
    Elo0: 0
    Elo1: 10
    H1 was accepted
    ```

--- src/timeman.hpp
+++ src/timeman.hpp
@@ -7,16 +7,18 @@ namespace Timeman {
 
 constexpr uint64_t INF_TIME = 1 << 28;
 
-struct TimeManager
+class TimeManager
 {
-    TimeManager() : TimeManager(INF_TIME, 0){};
-    TimeManager(uint64_t time, uint64_t inc);
-    bool hardTimeUp() const;
-    bool softTimeUp() const;
-    int64_t getTimeElapsed() const;
-    
-    std::chrono::system_clock::time_point startTime; 
-    int64_t hardTimeLimit, softTimeLimit;
+    public:
+        TimeManager() : TimeManager(INF_TIME, 0){};
+        TimeManager(uint64_t time, uint64_t inc);
+        bool hardTimeUp() const;
+        bool softTimeUp() const;
+        int64_t getTimeElapsed() const;
+
+    private:
+        std::chrono::system_clock::time_point startTime;
+        int64_t hardTimeLimit, softTimeLimit;
 };
 
 } // namespace Timeman
