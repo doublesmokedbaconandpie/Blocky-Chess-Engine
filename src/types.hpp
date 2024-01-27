7c04a45ba6c689753c66bacdc1660d51ee64de5c
Kevin Nguyen <kevinhnguyen@ucsb.edu>
2024-01-26 22:16:29 -0800

    extend FixedVector functionality

--- src/types.hpp
+++ src/types.hpp
@@ -92,26 +92,54 @@ constexpr inline castleRights& operator^=(castleRights& lhs, castleRights rhs) {
 }
 
 // internally uses a fixed size array to avoid memory reallocations, like what can happen in std::vector
+// there is no bounds checking, so be careful with push and pop backs
 template<typename T, std::size_t N>
 class FixedVector {
     public:
-        constexpr T& operator[](int index) {
+        // writing
+        constexpr T& operator[](std::size_t index) {
+            return this->container[index];
+        }
+        // reading
+        constexpr const T& operator[](std::size_t index) const {
             return this->container[index];
         }
         constexpr void push_back(const T& entry) {
-            this->container[this->vecSize] = entry;
-            this->vecSize++;
+            this->pointer++;
+            this->container[this->pointer] = entry;
+        }
+        constexpr void pop_back() {
+            // there is no need to null initialize the old memory since push_back is the only valid way to access it
+            this->pointer--;
+        }
+        constexpr T back() {
+            return this->container[this->pointer];
         }
         constexpr auto begin() const {
             return this->container.begin();
         }
         constexpr auto end() const {
-            return this->container.begin() + this->vecSize;
+            return this->container.begin() + this->pointer + 1;
         }
         constexpr auto size() const {
-            return this->vecSize;
+            return this->pointer + 1;
+        }
+        constexpr auto clear() {
+            // there is no need to null initialize the old memory since push_back is the only valid way to access it
+            this->pointer = -1;
+        }
+        constexpr bool operator==(const FixedVector<T, N>& rhs) const {
+            if (this->size() != rhs.size()) {
+                return false;
+            }
+            for (std::size_t i = 0; i < this->size(); ++i) {
+                if (this->container[i] != rhs[i]) {
+                    return false;
+                }
+            }
+            return true;
         }
     private:
         std::array<T, N> container{};
-        std::size_t vecSize{};
+        std::size_t pointer = -1;
 };
