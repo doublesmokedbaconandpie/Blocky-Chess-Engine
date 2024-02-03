24b5d4e6952579f901714bf69804506b6fafe4b9
Kevin Nguyen <kevinhnguyen@ucsb.edu>
2024-02-03 10:18:38 -0800

    allow phalanx pawns to be tuned

--- tools/tune/texelBlocky.cpp
+++ tools/tune/texelBlocky.cpp
@@ -36,6 +36,7 @@ parameters_t BlockyEval::get_initial_parameters() {
     // misc evaluation terms
     pushSingleTerm(params, "doubledPawns", Eval::doubledPawns);
     pushSingleTerm(params, "chainedPawns", Eval::chainedPawns);
+    pushSingleTerm(params, "phalanxPawns", Eval::phalanxPawns);
 
     totalSize = params.size();
     return params;
@@ -103,16 +104,18 @@ EvalResult BlockyEval::get_fen_eval_result(const std::string& fen) {
         const int pieceOffset = colorlessPiece * BOARD_SIZE;
 
         // pawn terms
-        int passedPawnFlag = 0, doubledPawnFlag = 0, chainedPawnFlag = 0;
+        int passedPawnFlag = 0, doubledPawnFlag = 0, chainedPawnFlag = 0, phalanxPawnFlag = 0;
         if (colorlessPiece == WPawn) {
             const uint64_t allyPawnSet = isWhitePiece ? board.pieceSets[WPawn] : board.pieceSets[BPawn];
             const uint64_t enemyPawnSet = isWhitePiece ? board.pieceSets[BPawn] : board.pieceSets[WPawn];
             const uint64_t doubledPawnSet = Eval::getDoubledPawnsMask(allyPawnSet, isWhitePiece);
             const uint64_t chainedPawnSet = Eval::getChainedPawnsMask(allyPawnSet, isWhitePiece);
+            const uint64_t phalanxPawnSet = Eval::getPhalanxPawnsMask(allyPawnSet, isWhitePiece);
 
             passedPawnFlag = static_cast<int>(Eval::isPassedPawn(i, enemyPawnSet, isWhitePiece));
             doubledPawnFlag = static_cast<int>(static_cast<bool>(doubledPawnSet & c_u64(1) << i));
             chainedPawnFlag = static_cast<int>(static_cast<bool>(chainedPawnSet & c_u64(1) << i));
+            phalanxPawnFlag = static_cast<int>(static_cast<bool>(phalanxPawnSet & c_u64(1) << i));
         }
 
         // white and black pieces use different eval indices in piece square tables
@@ -126,6 +129,7 @@ EvalResult BlockyEval::get_fen_eval_result(const std::string& fen) {
         result.coefficients[offsets["passedPawns"] + rankOffset] += passedPawnFlag * occurences;
         result.coefficients[offsets["doubledPawns"]] += doubledPawnFlag * occurences;
         result.coefficients[offsets["chainedPawns"]] += chainedPawnFlag * occurences;
+        result.coefficients[offsets["phalanxPawns"]] += phalanxPawnFlag * occurences;
     }
 
     result.score = board.evaluate();
